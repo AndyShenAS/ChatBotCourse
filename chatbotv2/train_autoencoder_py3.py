f6e33703e6ec386fc9a51391d9b3f38f547fbbac
AndyShenAS <discovery.yuan@qq.com>
2018-05-15 20:51:19 +0900

    ll

--- chatbotv2/train_autoencoder_py3.py
+++ chatbotv2/train_autoencoder_py3.py
@@ -1070,14 +1070,14 @@ def predict(input_sentence = '我肚子好饿饿哦'):
 
 
 # sentences = ['我喜欢你','我爱你','我讨厌你','我恨你','你喜欢我','你爱我','你讨厌我','你恨我']
-# sentences = ['我喜欢你','我爱你','我羡慕你','你喜欢我','你爱我','你羡慕我']
-# sentences_dic = {}
-# for sentence in sentences:
-#     sentences_dic[sentence] = predict(sentence)
-#
-# f = open('./data/sentence_vectors.dic','w')
-# f.write(str(sentences_dic))
-# f.close()
+sentences = ['我喜欢你','我爱你','我羡慕你','你喜欢我','你爱我','你羡慕我']
+sentences_dic = {}
+for sentence in sentences:
+    sentences_dic[sentence] = predict(sentence)
+
+f = open('./data/sentence_vectors.dic','w')
+f.write(str(sentences_dic))
+f.close()
 
 
 
--- word_segment_py3.py
+++ word_segment_py3.py
@@ -10,20 +10,21 @@ from jieba import analyse
 def segment(input, output):
     input_file = open(input, "r")
     output_file = open(output, "w")
+    segments = ""
     while True:
         line = input_file.readline()
         if line:
             line = line.strip()
             seg_list = jieba.cut(line)
-            segments = ""
+            segment = ''
             # segments = "GO_ID"
-            for str in seg_list:
-                segments = segments + " " + str
-            # segments = segments + " EOS_ID"+"\n"
-            segments = segments + "\n"
-            output_file.write(segments)
+            for seg in seg_list:
+                if seg != '\n':
+                    segment += seg + " "
+            segments += segment + "\n"
         else:
             break
+    output_file.write(segments)
     input_file.close()
     output_file.close()
 
@@ -49,5 +50,7 @@ if __name__ == '__main__':
     # segment('chatbotv5/samples/answer.big.norepeat', 'chatbotv5/samples/answer.big.norepeat.segment')
     # segment('chatbotv5/samples/backup/question', 'chatbotv5/samples/backup/question.segment')
     # segment('chatbotv5/samples/backup/answer', 'chatbotv5/samples/backup/answer.segment')
-    segment('chatbotv2/data/question.all.norepeat', 'chatbotv2/data/question.all.norepeat.segment')
-    segment('chatbotv2/data/answer.all.norepeat', 'chatbotv2/data/answer.all.norepeat.segment')
+    # segment('chatbotv2/data/question.all.norepeat', 'chatbotv2/data/question.all.norepeat.segment')
+    # segment('chatbotv2/data/answer.all.norepeat', 'chatbotv2/data/answer.all.norepeat.segment')
+    # segment('./chatbotv5/samples/answer', './chatbotv5/samples/answer.segment')
+    segment('./chatbotv5/samples/question', './chatbotv5/samples/question.segment')
